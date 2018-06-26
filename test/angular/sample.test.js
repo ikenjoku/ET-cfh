232576ee7c0c777563257383222686c8465e9018
Folajimi Ogunbadejo <bekomay26@gmail.com>
2018-06-26 15:12:29 +0100

    chore(config): Setup unit testing for front end
    - install jasmine and karma
    - Configure karma to use the jasmine framework
    - Configure karma to launch chrome when the test is started
    - Write sample test
    
    [Finishes #158457030]

---
+++ test/angular/sample.test.js
@@ -0,0 +1,11 @@
+/**
+ * Sample test
+ */
+describe("Sample test to ensure karma config works", () => {
+  it('should pass', () => {
+    expect(1).toEqual(1);
+  });
+  it('should fail', () => {
+    expect(2).toEqual(1);
+  });
+});
